38f02f52967e26c47568989db993a89f60212ee4
caicia <2564926167@qq.com>
2018-12-18 14:55:35 +0800

    face更新

---
+++ app/src/main/java/com/example/cc/iocontrolapplication/common/Constants.java
@@ -0,0 +1,6 @@
+package com.example.cc.iocontrolapplication.common;
+
+public class Constants {
+    public static final String APP_ID = "EMPDR3SsAJCQ4mH7EAzrrYSQUWQAvE6AqPUwWquVUZN7";
+    public static final String SDK_KEY = "Dj8UEaBzweVym9eSsR3suTc9GN388pkzxShawPjRWNwr";
+}
